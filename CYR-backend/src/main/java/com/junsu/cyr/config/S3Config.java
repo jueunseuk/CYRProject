313e90625e3314dcc835d369120c7238c3d9c5be
jueunseuk <junsu120202@gmail.com>
2025-03-11 01:32:11 +0900

    #feat : user entity setting

---
+++ CYR-backend/src/main/java/com/junsu/cyr/config/S3Config.java
@@ -0,0 +1,27 @@
+package com.junsu.cyr.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+
+    private String accessKey = "AKIA34AMCZPTY3VB5EO7";
+
+    private String secretKey = "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";
+
+    private String region = "ap-northeast-1";
+
+    @Bean
+    public S3Client s3Client() {
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+        return S3Client.builder()
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .region(Region.of(region))
+                .build();
+    }
+}
---
+++ CYR-backend/src/main/java/com/junsu/cyr/domain/users/Gender.java
@@ -0,0 +1,5 @@
+package com.junsu.cyr.domain.users;
+
+public enum Gender {
+    MALE, FEMALE, NEITHER;
+}

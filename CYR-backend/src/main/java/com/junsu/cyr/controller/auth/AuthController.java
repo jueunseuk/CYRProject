85e64ae3d81f1fc335d72723d19c4a1b14444d15
jueunseuk <junsu120202@gmail.com>
2025-03-14 02:05:25 +0900

    #feature : implements email authentication

--- CYR-backend/src/main/java/com/junsu/cyr/controller/auth/AuthController.java
+++ CYR-backend/src/main/java/com/junsu/cyr/controller/auth/AuthController.java
@@ -1,40 +1,34 @@
 package com.junsu.cyr.controller.auth;
 
+import com.junsu.cyr.model.email.EmailCodeRequest;
+import com.junsu.cyr.model.email.EmailMatchRequest;
 import com.junsu.cyr.response.exception.BaseException;
-import com.junsu.cyr.response.exception.code.MailExceptionCode;
+import com.junsu.cyr.response.exception.code.EmailExceptionCode;
 import com.junsu.cyr.response.success.SuccessResponse;
-import com.junsu.cyr.service.auth.AuthService;
 import com.junsu.cyr.service.auth.MailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
 
-    private final AuthService authService;
     private final MailService mailService;
 
-    @ResponseBody
     @PostMapping("/email/request")
-    public ResponseEntity<?> mailSend(@RequestParam String email){
-        String.valueOf(mailService.sendMail(email));
+    public ResponseEntity<?> mailSend(@RequestBody EmailCodeRequest request){
+        mailService.sendMail(request.getEmail());
         return ResponseEntity.ok(SuccessResponse.success("Success to request authentication code"));
     }
 
-    @ResponseBody
     @PostMapping("/email/check")
-    public ResponseEntity<?> codeCheck(@RequestParam String email, @RequestParam String code) {
-        if(mailService.verifyCode(email, code)){
+    public ResponseEntity<?> codeCheck(@RequestBody EmailMatchRequest request) {
+        if(mailService.verifyCode(request.getEmail(), request.getCode())){
             return ResponseEntity.ok(SuccessResponse.success("Matches with authentication code"));
         } else{
-            throw new BaseException(MailExceptionCode.UNMATCHED_AUTHENTICATION_CODE);
+            throw new BaseException(EmailExceptionCode.UNMATCHED_AUTHENTICATION_CODE);
         }
     }
 }
--- CYR-backend/src/main/java/com/junsu/cyr/response/ApiResponse.java
+++ CYR-backend/src/main/java/com/junsu/cyr/response/ApiResponse.java
@@ -1,5 +1,8 @@
 package com.junsu.cyr.response;
 
+import lombok.Getter;
+
+@Getter
 public abstract class ApiResponse {
     private final Boolean isSuccess;
     private final String code;

12878ad1bd353e7259a7aeb977bd141151c27984
jueunseuk <junsu120202@gmail.com>
2025-03-18 19:04:19 +0900

    #feature : use shouldNotFilter for auth

--- src/main/java/com/junsu/cyr/config/SecurityConfig.java
+++ src/main/java/com/junsu/cyr/config/SecurityConfig.java
@@ -1,3 +1,4 @@
+
 package com.junsu.cyr.config;
 
 import com.junsu.cyr.util.JwtAuthenticationFilter;
@@ -34,13 +35,7 @@ public class SecurityConfig {
                     return config;
                 }))
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                        .requestMatchers(
-                                "/auth/naver",
-                                "/auth/signup",
-                                "/auth/login",
-                                "/auth/token/access",
-                                "/auth/email/request",
-                                "/auth/email/check")
+                        .requestMatchers(SecurityConstant.PERMIT_ENDPOINTS.toArray(new String[0]))
                         .permitAll()
                         .anyRequest()
                         .authenticated()

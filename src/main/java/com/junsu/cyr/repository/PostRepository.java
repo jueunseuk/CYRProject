6b591a5cab57ae26271070617b5e6afb6d3a7584
jueunseuk <junsu120202@gmail.com>
2025-03-31 15:03:38 +0900

    #fix: popular post list improve

--- src/main/java/com/junsu/cyr/repository/PostRepository.java
+++ src/main/java/com/junsu/cyr/repository/PostRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
@@ -15,4 +17,12 @@ public interface PostRepository extends JpaRepository<Post, Integer> {
 
     @Query("SELECT p FROM Post p WHERE p.board.boardId BETWEEN :start AND :end")
     Page<Post> findAllNew(Integer start, Integer end, Pageable pageable);
+
+    @Query("SELECT p FROM Post p WHERE p.board.boardId BETWEEN :startId AND :endId " +
+            "AND p.createdAt BETWEEN :start AND :end")
+    Page<Post> findPopularPostsWithinDates(@Param("startId") Integer startId,
+                                           @Param("endId") Integer endId,
+                                           @Param("start") LocalDateTime start,
+                                           @Param("end") LocalDateTime end,
+                                           Pageable pageable);
 }

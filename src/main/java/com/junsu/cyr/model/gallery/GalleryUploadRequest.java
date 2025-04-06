9236c0a66d1bff2951c9e6f04d0a120c6a77ad2d
jueunseuk <junsu120202@gmail.com>
2025-04-06 14:31:43 +0900

    #fix: gallery picturedAt type fix

--- src/main/java/com/junsu/cyr/model/gallery/GalleryUploadRequest.java
+++ src/main/java/com/junsu/cyr/model/gallery/GalleryUploadRequest.java
@@ -4,7 +4,6 @@ import com.junsu.cyr.domain.images.Type;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
@@ -12,7 +11,7 @@ public class GalleryUploadRequest {
     private String title;
     private String description;
     private Type type;
-    private LocalDateTime picturedAt;
+    private String picturedAt;
 
     private List<MultipartFile> images;
 }

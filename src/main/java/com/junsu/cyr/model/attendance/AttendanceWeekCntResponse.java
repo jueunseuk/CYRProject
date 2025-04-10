e077f116969b8498b06be6bc9d9f70c0c9a8e329
jueunseuk <junsu120202@gmail.com>
2025-04-10 17:13:38 +0900

    #fix: date calc login

--- src/main/java/com/junsu/cyr/model/attendance/AttendanceWeekCntResponse.java
+++ src/main/java/com/junsu/cyr/model/attendance/AttendanceWeekCntResponse.java
@@ -2,7 +2,7 @@ package com.junsu.cyr.model.attendance;
 
 import lombok.Data;
 
-import java.util.List;
+import java.util.Map;
 
 @Data
 public class AttendanceWeekCntResponse {
@@ -18,17 +18,17 @@ public class AttendanceWeekCntResponse {
     private Integer friday;
     private Integer saturday;
 
-    public AttendanceWeekCntResponse(Integer thisMonthCnt, Integer beforeMonthCnt, Integer thisWeekCnt, Integer beforeWeekCnt, List<Integer> cnt) {
+    public AttendanceWeekCntResponse(Integer thisMonthCnt, Integer beforeMonthCnt, Integer thisWeekCnt, Integer beforeWeekCnt, Map<String, Integer> list) {
         this.thisMonthCnt = thisMonthCnt;
         this.beforeMonthCnt = beforeMonthCnt;
         this.thisWeekCnt = thisWeekCnt;
         this.beforeWeekCnt = beforeWeekCnt;
-        this.sunday = cnt.get(0);
-        this.monday = cnt.get(1);
-        this.tuesday = cnt.get(2);
-        this.wednesday = cnt.get(3);
-        this.thursday = cnt.get(4);
-        this.friday = cnt.get(5);
-        this.saturday = cnt.get(6);
+        this.sunday = list.get("sunday");
+        this.monday = list.get("monday");
+        this.tuesday = list.get("tuesday");
+        this.wednesday = list.get("wednesday");
+        this.thursday = list.get("thursday");
+        this.friday = list.get("friday");
+        this.saturday = list.get("saturday");
     }
 }

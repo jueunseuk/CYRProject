876b84e3f52f846f4e98f3fd779c293e4c2dc699
jueunseuk <junsu120202@gmail.com>
2025-09-24 13:54:55 +0900

    temp

---
+++ src/main/java/com/junsu/cyr/service/comment/CommentService.java
@@ -0,0 +1,9 @@
+package com.junsu.cyr.service.comment;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+}

566c08bdb6f5a460b3b4f0b318ee6aca61371146
returns0825 <junsu120202@gmail.com>
2025-10-01 18:01:21 +0900

    #feat: other profile get API

---
+++ src/main/java/com/junsu/cyr/model/user/OtherProfileResponse.java
@@ -0,0 +1,42 @@
+package com.junsu.cyr.model.user;
+
+import com.junsu.cyr.domain.users.Gender;
+import com.junsu.cyr.domain.users.Method;
+import com.junsu.cyr.domain.users.Role;
+import com.junsu.cyr.domain.users.User;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class OtherProfileResponse {
+    private Integer userId;
+    private String name;
+    private String nickname;
+    private String email;
+    private String profileUrl;
+    private String introduction;
+    private Gender gender;
+    private Integer age;
+    private Long expCnt;
+    private Method method;
+    private Integer attendanceCnt;
+    private Role role;
+    private LocalDateTime createdAt;
+
+    public OtherProfileResponse(User user) {
+        this.userId = user.getUserId();
+        this.name = user.getName();
+        this.nickname = user.getNickname();
+        this.email = user.getEmail();
+        this.profileUrl = user.getProfileUrl();
+        this.introduction = user.getIntroduction();
+        this.gender = user.getGender();
+        this.age = user.getAge();
+        this.expCnt = user.getEpxCnt();
+        this.method = user.getMethod();
+        this.attendanceCnt = user.getAttendanceCnt();
+        this.role = user.getRole();
+        this.createdAt = user.getCreatedAt();
+    }
+}
--- src/main/java/com/junsu/cyr/service/user/UserService.java
+++ src/main/java/com/junsu/cyr/service/user/UserService.java
@@ -4,6 +4,7 @@ import com.junsu.cyr.domain.experiences.Experience;
 import com.junsu.cyr.domain.sand.Sand;
 import com.junsu.cyr.domain.temperature.Temperature;
 import com.junsu.cyr.domain.users.User;
+import com.junsu.cyr.model.user.OtherProfileResponse;
 import com.junsu.cyr.model.user.UserProfileResponse;
 import com.junsu.cyr.model.user.UserSidebarResponse;
 import com.junsu.cyr.repository.UserRepository;
@@ -27,11 +28,6 @@ public class UserService {
 
     public User getUserById(Integer userId) {
         return userRepository.findById(userId).orElseThrow(() -> new BaseException(UserExceptionCode.NOT_EXIST_USER));
-    }
-
-    public Long getUserExp(Integer userid) {
-        User user = getUserById(userid);
-        return user.getEpxCnt();
     }
 
     public UserSidebarResponse getUserSidebar(Integer userId) {
@@ -72,5 +68,10 @@ public class UserService {
         user.updateTemperature(temperature.getAmount());
 
         temperatureService.createTemperatureLog(user, temperature);
+    }
+
+    public OtherProfileResponse getOtherProfile(Integer otherId) {
+        User user = getUserById(otherId);
+        return new OtherProfileResponse(user);
     }
 }

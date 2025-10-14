45620d9e042b9388501c10120f0faa4aabf1f413
returns0825 <junsu120202@gmail.com>
2025-10-14 19:50:46 +0900

    #feat: glass convert api implement

--- src/main/java/com/junsu/cyr/response/exception/code/GlassExceptionCode.java
+++ src/main/java/com/junsu/cyr/response/exception/code/GlassExceptionCode.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum GlassExceptionCode implements ExceptionCode {
     NOT_FOUND_GLASS("GLASSL_001", "해당 유리 획득 경로는 존재하지 않습니다.", HttpStatus.NOT_FOUND),
+    NOT_ENOUGH_SAND("GLASS_002", "모래알의 수량이 부족합니다.", HttpStatus.BAD_REQUEST),
+    NOT_ENOUGH_TEMPERATURE("GLASS_003", "활동 온도가 불충분합니다.", HttpStatus.BAD_REQUEST),
     ;
 
     private final String code;

f6ac634ba141aaded0922a91e3a3abeab0c2d10a
jueunseuk <junsu120202@gmail.com>
2025-10-17 12:52:01 +0900

    #hotfix: time zone issue

--- src/main/java/com/junsu/cyr/model/calendar/CalendarResponse.java
+++ src/main/java/com/junsu/cyr/model/calendar/CalendarResponse.java
@@ -12,6 +12,9 @@ public class CalendarResponse {
     private Type type;
     private String title;
     private String description;
+    private String link1;
+    private String link2;
+    private String imageUrl;
     private String location;
     private LocalDate date;
 
@@ -20,6 +23,9 @@ public class CalendarResponse {
         this.type = calendar.getType();
         this.title = calendar.getTitle();
         this.description = calendar.getDescription();
+        this.link1 = calendar.getLink1();
+        this.link2 = calendar.getLink2();
+        this.imageUrl = calendar.getImageUrl();
         this.location = calendar.getLocation();
         this.date = calendar.getDate();
     }

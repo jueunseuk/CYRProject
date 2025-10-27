c4d050407579289ed113c5508f7a92034119d4fe
returns0825 <junsu120202@gmail.com>
2025-10-27 10:36:24 +0900

    #hotfix: get other chat room list

--- src/main/java/com/junsu/cyr/repository/ChatRoomRepository.java
+++ src/main/java/com/junsu/cyr/repository/ChatRoomRepository.java
@@ -4,7 +4,11 @@ import com.junsu.cyr.domain.chats.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
     ChatRoom findByChatRoomId(Long chatRoomId);
+
+    List<ChatRoom> findAllByChatRoomIdNotIn(List<Long> chatRooms);
 }

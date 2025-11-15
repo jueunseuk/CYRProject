d04eca125e9775136dd1c76f178442846d1e602b
returns0825 <junsu120202@gmail.com>
2025-11-16 02:28:40 +0900

    #feat: achievement domain design

--- src/main/java/com/junsu/cyr/domain/achievements/Achievement.java
+++ src/main/java/com/junsu/cyr/domain/achievements/Achievement.java
@@ -20,34 +20,19 @@ public class Achievement {
     @Column(name = "achievement_id", nullable = false)
     private Integer achievementId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "achievement_next")
-    private Achievement achievementNext;
-
     @Column(name = "name", nullable = false, length = 1000)
     private String name;
 
-    @Column(name = "badge_url")
-    private String badgeUrl;
+    @Column(name = "description", nullable = false, length = 1000)
+    private String description;
 
-    @Column(name = "reward_sand")
-    private Integer rewardSand;
+    @Column(name = "image_url")
+    private String imageUrl;
 
-    @Column(name = "condition_number")
-    private Integer conditionNumber;
-
-    @Column(name = "condition_date")
-    private LocalDateTime conditionDate;
+    @Column(name = "condition_amount")
+    private Integer conditionAmount;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "type", nullable = false)
     private Type type;
-
-    @Enumerated(EnumType.STRING)
-    @Column(name = "category", nullable = false)
-    private Category category;
-
-    @Enumerated(EnumType.STRING)
-    @Column(name = "condition_type", nullable = false)
-    private ConditionType conditionType;
 }

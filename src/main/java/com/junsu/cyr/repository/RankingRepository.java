143bb88bc799cb762a75703bcf69f78baa8cdb37
returns0825 <junsu120202@gmail.com>
2025-11-18 09:24:54 +0900

    #fix: notification policy

--- src/main/java/com/junsu/cyr/repository/RankingRepository.java
+++ src/main/java/com/junsu/cyr/repository/RankingRepository.java
@@ -4,6 +4,7 @@ import com.junsu.cyr.domain.rankings.Ranking;
 import com.junsu.cyr.domain.rankings.RankingCategory;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,5 +15,6 @@ public interface RankingRepository extends JpaRepository<Ranking, Integer> {
 
     List<Ranking> findAllByRankingCategory(RankingCategory rankingCategory, Pageable pageable);
 
+    @Query("select r.user.userId from Ranking r")
     List<Integer> findUser_UserId();
 }

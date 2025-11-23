6626150e1a40751e7daee89b9b862a391fb2e2d8
returns0825 <junsu120202@gmail.com>
2025-11-23 14:50:08 +0900

    #refactor: get post list

--- src/main/java/com/junsu/cyr/response/exception/code/BoardExceptionCode.java
+++ src/main/java/com/junsu/cyr/response/exception/code/BoardExceptionCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum BoardExceptionCode implements ExceptionCode {
     NOT_FOUND_BOARD("BOARD_001", "해당 게시판을 찾지 못했습니다.", HttpStatus.NOT_FOUND),
-
+    NOT_FOUND_BOARD_ID("BOARD_002", "게시판 아이디가 존재하지 않습니다.", HttpStatus.BAD_REQUEST),
     ;
 
     private final String code;
--- src/main/java/com/junsu/cyr/service/post/PostService.java
+++ src/main/java/com/junsu/cyr/service/post/PostService.java
@@ -16,6 +16,7 @@ import com.junsu.cyr.model.search.SearchConditionRequest;
 import com.junsu.cyr.repository.CommentRepository;
 import com.junsu.cyr.repository.EmpathyRepository;
 import com.junsu.cyr.repository.PostRepository;
+import com.junsu.cyr.response.exception.code.BoardExceptionCode;
 import com.junsu.cyr.response.exception.code.UserExceptionCode;
 import com.junsu.cyr.response.exception.http.BaseException;
 import com.junsu.cyr.response.exception.code.PostExceptionCode;
@@ -24,6 +25,7 @@ import com.junsu.cyr.service.achievement.AchievementService;
 import com.junsu.cyr.service.board.BoardService;
 import com.junsu.cyr.service.comment.CommentService;
 import com.junsu.cyr.service.user.UserService;
+import com.junsu.cyr.util.PageableMaker;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 import lombok.RequiredArgsConstructor;
@@ -90,88 +92,15 @@ public class PostService {
     }
 
     public Page<PostListResponse> getPosts(PostSearchConditionRequest condition) {
-        String jpql = getCondition(condition);
-
-        Pageable pageable = PageRequest.of(condition.getPage(), condition.getSize(), Sort.by(condition.getSort()).descending());
-
-        TypedQuery<Post> query = entityManager.createQuery(jpql, Post.class);
-
-        if(condition.getBoardId() != null) {
-            query.setParameter("boardId", condition.getBoardId());
+        if(condition.getBoardId() == null) {
+            throw new BaseException(BoardExceptionCode.NOT_FOUND_BOARD_ID);
         }
 
-        if(condition.getTitle() != null && !condition.getTitle().isEmpty()) {
-            query.setParameter("title", "%"+condition.getTitle()+"%");
-        }
+        Board board = boardService.findBoardByBoardId(condition.getBoardId());
 
-        if(condition.getUserNickname() != null && !condition.getUserNickname().isEmpty()) {
-            query.setParameter("nickname", condition.getUserNickname()+"%");
-        }
+        Page<Post> posts = postRepository.findAllByBoard(board, PageableMaker.of(condition.getPage(), condition.getSize(), condition.getSort(), condition.getDirection()));
 
-        if(condition.getStart() != null && condition.getEnd() != null) {
-            query.setParameter("start", condition.getStart());
-            query.setParameter("end", condition.getEnd());
-        }
-
-        query.setFirstResult((condition.getPage()) * condition.getSize());
-        query.setMaxResults(condition.getSize());
-
-        List<Post> resultList = query.getResultList();
-
-        String countJpql = jpql.replaceFirst("SELECT p", "SELECT COUNT(p)");
-        TypedQuery<Long> countQuery = entityManager.createQuery(countJpql, Long.class);
-
-        if(condition.getBoardId() != null) {
-            countQuery.setParameter("boardId", condition.getBoardId());
-        }
-
-        if(condition.getTitle() != null && !condition.getTitle().isEmpty()) {
-            countQuery.setParameter("title", "%"+condition.getTitle()+"%");
-        }
-
-        if(condition.getUserNickname() != null && !condition.getUserNickname().isEmpty()) {
-            countQuery.setParameter("nickname", condition.getUserNickname()+"%");
-        }
-
-        if(condition.getStart() != null && condition.getEnd() != null) {
-            countQuery.setParameter("start", condition.getStart());
-            countQuery.setParameter("end", condition.getEnd());
-        }
-
-        Long totalCount = countQuery.getSingleResult();
-
-        List<PostListResponse> postListResponses = resultList.stream()
-                .map(PostListResponse::new)
-                .toList();
-
-        return new PageImpl<>(postListResponses, pageable, totalCount);
-    }
-
-    public String getCondition(PostSearchConditionRequest condition) {
-        StringBuilder jpql = new StringBuilder("SELECT p FROM Post p WHERE 1=1");
-
-        if (condition.getBoardId() != null) {
-            jpql.append(" AND p.board.boardId = :boardId");
-        }
-
-        if(condition.getTitle() != null && !condition.getTitle().isEmpty()) {
-            jpql.append(" AND p.title LIKE :title");
-        }
-
-        if(condition.getUserNickname() != null && !condition.getUserNickname().isEmpty()) {
-            jpql.append(" AND p.userId.nickname LIKE :nickname");
-        }
-
-        if(condition.getStart() != null && condition.getEnd() != null) {
-            jpql.append(" AND p.createdAt BETWEEN :start AND :end");
-        }
-
-        jpql.append(" ORDER BY p.")
-                .append(condition.getSort())
-                .append(" ")
-                .append(condition.getDirection());
-
-        return jpql.toString();
+        return posts.map(PostListResponse::new);
     }
 
     @Transactional

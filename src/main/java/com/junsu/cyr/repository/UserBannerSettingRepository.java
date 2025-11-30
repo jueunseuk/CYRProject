e6399c82d4fc83f4a18fb077b7bc1128dc597be4
returns0825 <junsu120202@gmail.com>
2025-11-30 17:38:51 +0900

    #fix

--- src/main/java/com/junsu/cyr/repository/UserBannerSettingRepository.java
+++ src/main/java/com/junsu/cyr/repository/UserBannerSettingRepository.java
@@ -3,12 +3,14 @@ package com.junsu.cyr.repository;
 import com.junsu.cyr.domain.users.User;
 import com.junsu.cyr.domain.users.UserBannerSetting;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface UserBannerSettingRepository extends JpaRepository<UserBannerSetting, Long> {
+    @Query("select max(ubs.sequence) from UserBannerSetting ubs where ubs.user = :user")
     Integer findMaxSequenceByUser(User user);
 
     List<UserBannerSetting> findAllByUser(User user);
